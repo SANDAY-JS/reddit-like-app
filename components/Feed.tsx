89f70736c1b22d00740e06a754a598dcc9eceef8
SANDAY-JS <san.sh.s3@outlook.jp>
2022-07-23 21:50:56 +0900

    Added loadination in Feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -8,14 +8,18 @@ type Props = {
 }
 
 function Feed({topic}: Props) {
-  const {data, error} = 
+  const {data, loading, error} = 
     !topic ? useQuery(GET_ALL_POSTS) : 
     useQuery(GET_ALL_POSTS_BY_TOPIC, {variables: {topic: topic}})
 
   const posts: Post[] = !topic ? data?.getPostList : data?.getPostListByTopic;
 
+  if(loading) {
+    return <div className='mt-5 space-y-4 min-w-full sm:min-w-[400px] sm:flex-1 min-h-screen' />
+  }
+
   return (
-    <div className='mt-5 space-y-4 min-w-full sm:min-w-[400px]'>
+    <div className='mt-5 space-y-4 min-w-full sm:min-w-[400px] sm:flex-1'>
       {posts?.map((post) => (
         <Post key={post.id} post={post} />
       ))}
